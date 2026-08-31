a4fc34180dbc4a4e5be19d4ce782f3e7a93ff2c5
Dakin <dakin@192.168.2.49>
2026-08-31 11:20:05 +0700

    thêm db vào backend bằng thư viện psycog2 và tài liệu về db (new server) được lưu trong .env

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
 
 students = [
     {
-        "id": 1,
         "name": "Nguyễn Văn A",
         "class_name": "12A1"
     },
     {
-        "id": 2,
         "name": "Trần Văn B",
         "class_name": "12A2"
     }
@@ -21,6 +21,40 @@ students = [
 class Student(BaseModel):
     name: str
     class_name: str
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "postgres",
+    "user": "postgres",
+    "password": "qlsv2026.db",
+    "port": 5432
+}
+
+@app.post("/students")
+def add_student(student: Student):
+    conn = psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
+    cur = conn.cursor()
+    
+    sql = """
+        INSERT INTO students (name,class_name)
+        VALUES (%s, %s)
+        RETURNING id, name, class_name;
+    """
+    try:
+        cur.execute(sql, (student.name, student.class_name))
+        new_student = cur.fetchone()
+        conn.commit()
+        return {"status": "success", "data": new_student}
+        
+    except psycopg2.IntegrityError:
+        conn.rollback()
+        raise HTTPException(status_code=400, detail="Email này đã tồn tại!")
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cur.close()
+        conn.close()
 
 
 @app.get("/")
